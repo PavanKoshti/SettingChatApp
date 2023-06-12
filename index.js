afcfe5b003b8951c77e9b95e4ff99024776976ef
Pavan <pavankoshti786@gmail.com>
2023-06-12 16:48:53 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app)
+const Port = 3000;
+
+http.listen(Port, () => {
+    console.log(`Server Running on Port : ${Port}`);
+})
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+
+
+// Socket 
+const io = require('socket.io')(http)
+
+io.on('connection', (socket) => {
+    console.log('Connected...')
+    socket.on('message', (msg) => {
+        socket.broadcast.emit('message', msg)
+    })
+
+})
